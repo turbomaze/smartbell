64a370bb4254a759043e14aa7c91bd758ac8f47a
Anthony Liu <anthonyscottliu@gmail.com>
2016-02-13 17:50:39 -0500

    Adds a dummy post request for testing.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Smartbell' });
 });
 
+router.post('/', function(req, res) {
+  var val = parseFloat(req.body.thing);
+  console.log(val);
+  res.send(''+(val*val)+'\n');
+});
+
 module.exports = router;
