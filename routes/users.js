0c7ae339103482ba521387be622d52273f8d1254
Anthony Liu <anthonyscottliu@gmail.com>
2016-02-14 00:05:38 -0500

    Makes a frontend demo.

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.render('user');
+});
+
+router.post('/:id/reps', function(req, res) {
+	var n = parseInt(req.body.n);
+	var ret = [];
+	var cum = 0;
+	for (var ai = 0; ai < 10; ai++) {
+		ret.push([ai, cum]);
+		cum += Math.random();
+	}
+	res.send({data: ret});
 });
 
 module.exports = router;
